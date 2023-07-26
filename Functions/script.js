be4ede434452a25cb2ec38f62a4c113ecdb11b5c
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2023-07-26 14:49:25 +0500

    functions

---
+++ Functions/script.js
@@ -0,0 +1,22 @@
+/*      FUNCTIONS    */
+
+function calculateAge(birthyear){
+    // console.log('this is just a string')
+    return 2023 - birthyear;
+}
+
+var ahmed = calculateAge(2003);
+var luke = calculateAge(1996);
+var Jane = calculateAge(1999);
+var Stephen = calculateAge(1994);
+
+console.log(ahmed, luke, Jane, Stephen)
+
+// A FUNCTION that has a return value is in its intermediate process means its needs a value to complete the function
+
+// A Function with a return value needs to be in a variable to store
+
+function yearsUntillRetirement(firstname, year){
+    var yearstoretire = 65 - ahmed;
+    console.log(john + 'will retire in' + 1990 + 'years')
+}
