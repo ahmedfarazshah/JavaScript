a85fa8fee91de42a44d069eee17f5cb5679d00fa
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2023-07-28 11:49:30 +0500

    functions

--- Functions/script.js
+++ Functions/script.js
@@ -1,22 +1,33 @@
 /*      FUNCTIONS    */
 
-function calculateAge(birthyear){
-    // console.log('this is just a string')
-    return 2023 - birthyear;
+// the parameter or argument in a function is something on which the work will be applied
+function calulateAge(birthYear){
+    return 2023 - birthYear;
 }
 
-var ahmed = calculateAge(2003);
-var luke = calculateAge(1996);
-var Jane = calculateAge(1999);
-var Stephen = calculateAge(1994);
+var ahmedAge  = calulateAge(2001)
+var steveAge  = calulateAge(1956)
+var janeAge  = calulateAge(1999)
 
-console.log(ahmed, luke, Jane, Stephen)
+console.log(ahmedAge,steveAge,janeAge)
 
 // A FUNCTION that has a return value is in its intermediate process means its needs a value to complete the function
 
 // A Function with a return value needs to be in a variable to store
 
-function yearsUntillRetirement(firstname, year){
-    var yearstoretire = 65 - ahmed;
-    console.log(john + 'will retire in' + 1990 + 'years')
+// we are creating a function will tells us years untill retirement
+
+function yearUntillRetire(firstName , years){
+    var age = calulateAge(years);
+    var retirementAge = 55 - age;
+    if(retirementAge>0){
+    console.log(firstName +' will retire in ' + retirementAge + ' years')
+    }else{
+        console.log(firstName + ' is already retired')
+    }
 }
+
+yearUntillRetire('Mike', 1990)
+yearUntillRetire('Steve', 1956)
+yearUntillRetire('Angela', 1999)
+yearUntillRetire('Jane', 2001)
