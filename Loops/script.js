db4f56b0a9503eb7eb7178997a3ac4222cd928de
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2023-08-16 13:11:50 +0500

    loops

---
+++ Loops/script.js
@@ -0,0 +1,62 @@
+var arr=[12,33,66,88,'ahmed'];
+
+
+//for loop
+for (y=0; y<arr.length;y++){
+    console.log(arr[y]);
+}
+
+//while loop
+
+var evenarr = [2,4,6,8];
+
+let i = 0;
+while(i<arr.length){
+    console.log(evenarr[i]);
+    i++;
+}
+
+//do while
+
+var oddnum=[1,3,5,7,9];
+
+let k = 0;
+do{
+    console.log(oddnum[k]);
+    k++;
+}while(k<oddnum.length)
+
+
+//for the whole sum of an array
+
+var wholenum=[0,1,3,4,6,7,8];
+let sum=0;
+for(let i =0; i <wholenum.length;i++){
+    sum =sum+wholenum[i];
+}
+console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
