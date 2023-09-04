4384b093be3d3c21716753e821e95a95aed45ced
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2023-09-04 16:33:44 +0500

    scopeChain_This

--- How_codeWorks/script.js
+++ How_codeWorks/script.js
@@ -36,3 +36,89 @@ listing();
 
 
 ///////////////////////////////////////////
+// Scoping 
+
+    // the whole is global scope
+var a = 'whats up ';
+first();
+
+function first(){       // the function first scope is to the end of the function with the addition of global scope 
+    var b = 'Mike,';
+    second();
+
+        function second(){      // the second scope with the first and global with in 
+            var c = ' this is John';
+                console.log(a+b+c);
+                third();
+        }
+}
+
+
+// difference between the global scope and the execution context
+
+
+var a = 'whats up ';
+first();
+
+function first(){       // the function first scope is to the end of the function with the addition of global scope 
+    var b = 'Mike,';
+    second();
+
+        function second(){      // the second scope with the first and global with in 
+            var c = ' this is John';
+                console.log(a+b+c);
+                third();
+        }
+}
+function third(){        // this is the third scope that only has global scope and itself due to the scoping and has its own execution context
+    let d = 'Jane';
+    // console.log(a+b+c+d);// it will throw error due to the different execution context and third function is not in the lexical(code written inside a something) scoping of second or third is only called from the second function
+    console.log(a+d);
+}
+
+
+
+///////////////////////////////////////////////////////
+//          THIS KEYWORD 
+
+
+console.log(this);  // this is an example of the window object as this is pointing to the window object
+
+var person = {
+    name: 'Mike',
+    gender: 'single',
+    year_ofBirth: 2000,
+    calculate : function(){
+        console.log(this)
+    }
+}
+person.calculate();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
